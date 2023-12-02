c2220be035e6a421cd5c407ab52ec57a85eb5ff9
MKT17 <152396111+MKT17@users.noreply.github.com>
2023-12-02 11:31:52 +0100

    Add files via upload

---
+++ kelani assignment.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In This Notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives:** 
+# 1. List popular languages for data science
+# 2. List commomly used tool data scienctist used
+# 3. Basic arithmetic 
+
+# Some of the popular languages that data scientists usre are:
+# 1. r language
+# 2. java
+# 3. C++
+
+# Some of the commonly used libraries used by Data scientists include: 
+# 1. Numpy
+# 2. Panda
+# 3. Matplotlib
+
+# ### Below are a few examples of evaluating arithmetic expressions in python
+
+# In[6]:
+
+
+#This a simple arithmetic expression to multiply then add integers#
+(3*4)+5
+
+
+# In[7]:
+
+
+#convert 200 minutes to hours by dividing by 60#
+minutes = 200
+hours = minutes / 60
+print(hours)
+
+
+# ## Author
+# Kelani
+
+# In[ ]:
+
+
+
+
